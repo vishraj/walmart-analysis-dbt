1eb2b0d3ff07bcf8246491e7986a49a8723039cb
Vish Raj <vish.rajg@gmail.com>
2024-03-06 22:43:59 -0800

    updated the setup scripts for setting up snowflake

--- s3_snowpipe_integration.sql
+++ s3_snowpipe_integration.sql
@@ -4,12 +4,16 @@ create or replace storage integration s3_snowflake_int
     STORAGE_PROVIDER = S3
     ENABLED = TRUE
     STORAGE_AWS_ROLE_ARN = 'arn:aws:iam::090491204170:role/snowflake-access-role'
-    STORAGE_ALLOWED_LOCATIONS = ('s3://vr-snowpipe-test/', 's3://walmart-raw-source-vr/')
+    STORAGE_ALLOWED_LOCATIONS = ('s3://walmart-raw-source-vr/');
 
 -- 2: get the user ARN and external ID from the integration object
 DESC integration s3_snowflake_int;
 
--- 3: create the file format object
+-- 3: create the database for file_formats, external stages and pipes
+create database if not exists MANAGE_DB;
+
+-- 4: create the file format object
+create schema if not exists MANAGE_DB.file_formats;
 create or replace file format MANAGE_DB.file_formats.csv_format
     type = csv
     field_delimiter = ','
@@ -18,6 +22,7 @@ create or replace file format MANAGE_DB.file_formats.csv_format
     empty_field_as_null = TRUE;
 
 -- 5: create the stage object with the integration and file format object
+create schema if not exists MANAGE_DB.EXTERNAL_STAGES;
 create or replace stage MANAGE_DB.EXTERNAL_STAGES.walmart_stores_stage
     URL = 's3://walmart-raw-source-vr/stores/'
     STORAGE_INTEGRATION = s3_snowflake_int
@@ -34,7 +39,7 @@ create or replace stage MANAGE_DB.EXTERNAL_STAGES.walmart_facts_stage
     FILE_FORMAT = MANAGE_DB.file_formats.csv_format;
 
 -- 6: create the schema for the pipe
-CREATE OR REPLACE SCHEMA MANAGE_DB.pipes
+CREATE OR REPLACE SCHEMA MANAGE_DB.pipes;
 
 -- 7: create the pipes
 CREATE OR REPLACE pipe MANAGE_DB.pipes.walmart_stores_pipe
